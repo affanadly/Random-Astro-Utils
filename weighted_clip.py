39248551de69a8ff8d8bc1d2ad08d61063b98252
Affan Adly <89124642+affanadly@users.noreply.github.com>
2022-05-08 20:50:46 +0800

    Update weighted_clip.py

--- weighted_clip.py
+++ weighted_clip.py
@@ -14,9 +14,9 @@ def weighted_clip(image, clip_image,outname):
     
 def __main__():
 	ps = ArgumentParser(description='Performs weighting on continuum images based on sensitivity map, analogous to sampling distributions in MCMC. (https://github.com/affanadly)')
-	ps.add_argument("image", help="image to clip")
-	ps.add_argument("clip_image", help="sensitivity map to clip by")
-	ps.add_argument("-o", "--outname", type=str, default="output.fits", help="output file name. This OVERWRITES existing image of the same name.")	
+	ps.add_argument('image', help='image to clip')
+	ps.add_argument('clip_image', help='sensitivity map to clip by')
+	ps.add_argument('-o', '--outname', type=str, default='output.fits', help='output file name. This OVERWRITES existing image of the same name.')	
 
 	args = ps.parse_args()
 
@@ -25,5 +25,5 @@ def __main__():
 		 outname=args.outname)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	__main__()
