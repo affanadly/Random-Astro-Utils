180455465bdb3d736144f46104344f1ff3e56e60
Affan Adly <89124642+affanadly@users.noreply.github.com>
2022-05-08 23:28:02 +0800

    Update weighted_clip.py

--- weighted_clip.py
+++ weighted_clip.py
@@ -3,27 +3,24 @@
 from astropy.io import fits
 import numpy as np
 from argparse import ArgumentParser
+    
+def __main__():
+    ps = ArgumentParser(description='Performs weighting on continuum images based on sensitivity map, analogous to sampling distributions in MCMC. (https://github.com/affanadly)')
+    ps.add_argument('image', help='image to clip')
+    ps.add_argument('clip_image', help='sensitivity map to clip by')
+    ps.add_argument('-o', '--outname', type=str, default='output.fits', help='output file name. This OVERWRITES existing image of the same name.')	
 
-def weighted_clip(image, clip_image,outname):
-    o = fits.open(image)
-    c = fits.open(clip_image)
+    args = ps.parse_args()
+
+    o = fits.open(args.image)
+    c = fits.open(args.clip_image)
     
     o[0].data /= c[0].data
     
-    fits.writeto(outname,o[0].data,o[0].header,overwrite=True)
+    o[0].header['history'] = f'Weight-clipped using weighted_clip.py with {args.clip_image} sensitivity map (https://github.com/affanadly)'
     
-def __main__():
-	ps = ArgumentParser(description='Performs weighting on continuum images based on sensitivity map, analogous to sampling distributions in MCMC. (https://github.com/affanadly)')
-	ps.add_argument('image', help='image to clip')
-	ps.add_argument('clip_image', help='sensitivity map to clip by')
-	ps.add_argument('-o', '--outname', type=str, default='output.fits', help='output file name. This OVERWRITES existing image of the same name.')	
-
-	args = ps.parse_args()
-
-	weighted_clip(image=args.image, 
-		 clip_image=args.clip_image, 
-		 outname=args.outname)
+    fits.writeto(args.outname,o[0].data,o[0].header,overwrite=True)
 
 
 if __name__ == '__main__':
-	__main__()
+    __main__()
