ee47ba03aa72c0eadb2c87e5c203bbc296e64a17
Affan Adly <89124642+affanadly@users.noreply.github.com>
2024-07-16 10:59:21 +0100

    Add files via upload

--- continuum_combine.py
+++ continuum_combine.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python
-
 from astropy.io import fits
 import numpy as np
 from argparse import ArgumentParser
 
-def combine(f): # f is list of HDUimages
+def combine(files,band,outname):
+    f = [fits.open(file) for file in files]
     data = np.array([chan[0].data for chan in f])
     shape = data[0].shape
-    return data.reshape(shape[0],-1,shape[2],shape[3])
+    data = data.reshape(shape[0],-1,shape[2],shape[3])
+
+    header = f[0][0].header
+    header['CRPIX3'] = 1
+    header['CRVAL3'] = band[0]
+    header['CDELT3'] = band[1]
+    header['NAXIS3'] = data.shape[2]
+    header['history'] = f'Combined {files} using continuum_combine.py (https://github.com/affanadly)'
     
+    fits.writeto(outname,data,header,overwrite=True)
+
 def __main__():
     ps = ArgumentParser(description='Combines continuum images into a pseudo-spectral image. (https://github.com/affanadly)')
     ps.add_argument('-f','--files', type=str, nargs='+', help='list of continuum images to combine.')
@@ -16,18 +25,12 @@ def __main__():
     ps.add_argument('-o', '--outname', type=str, default='output.fits', help='output file name. This OVERWRITES existing image of the same name.')	
 
     args = ps.parse_args()
-    
-    f = [fits.open(file) for file in args.files]
-    data = combine(f)
-    
-    header = f[0][0].header
-    header['CRPIX3'] = 1
-    header['CRVAL3'] = args.band[0]
-    header['CDELT3'] = args.band[1]
-    header['NAXIS3'] = data.shape[2]
-    header['history'] = f'Combined {args.files} using continuum_combine.py (https://github.com/affanadly)'
-    
-    fits.writeto(args.outname,data,header,overwrite=True)
-    
+
+    combine(
+        args.files,
+        args.band,
+        args.outname
+    )
+
 if __name__ == '__main__':
 	__main__()
